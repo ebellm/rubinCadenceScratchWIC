0019ce0ec9ac6b428d84fcd1a7eb50a2c2163eae
Alessandro <alessandro.mazzi@studenti.unipd.it>
2021-04-05 15:03:38 +0200

    Add class for Lallement's map.
    The class handles the intialization and querying of the map, both 2018
    and 2019, provided their respective files exist.

--- python/stilism_local.py
+++ python/stilism_local.py
@@ -10,24 +10,144 @@ import h5py
 import numpy as np
 from scipy import interpolate
 
-print("Setting up the map.")
-#lallement = h5py.File('stilism_cube.h5',mode='r')
-lallement = h5py.File('stilism_cube_2.h5',mode='r')
-# lallement = h5py.File('map3D_GAIAdr2_feb2019.h5',mode='r')
-stilism = lallement['stilism']
-cube = stilism['cube_datas']
-ebv = cube[:]
+# lallement18 => 'stilism_cube.h5' --- returns E(B-V)
+# lallement19 => 'stilism_cube_2.h5' --- returns A0
 
-steps = cube.attrs['gridstep_values']
-center = cube.attrs['sun_position'] * steps
+class LallementDustMap(object):
+    map_path = {'18':'stilism_cube.h5','19':'stilism_cube_2.h5'}
+    EBV2A = {'18': 3.1, '19': 1}
+    A2EBV = {'18': 1, '19': 1/3.1}
 
-x0 = np.arange(ebv.shape[0])*steps[0] - center[0]
-y0 = np.arange(ebv.shape[1])*steps[1] - center[1]
-z0 = np.arange(ebv.shape[2])*steps[2] - center[2]
+    def __init__(self, version='19'):
+        """Initialize Lallement's dust map.
+        Beware that the version 2018 of the map has E(B-V) values while the 2019 one has
+        A0 values.
 
-rgi = interpolate.RegularGridInterpolator((x0,y0,z0),ebv, \
-                                          bounds_error=False, fill_value=0.)
-print("Setup complete.")
+        Args:
+            version (str): Either '18' (map of E(B-V)) or '19' (map of A0).
+        """
+        if version not in self.map_path:
+            message = "The version of the map must be either one of {}"\
+                        .format(", ".join(self.map_path.keys()))
+            raise ValueError(message)
+        self.version = version
+        map_path = self.map_path[self.version]
+        print("Setting up the map {}.".format(map_path))
+        dust_map = h5py.File(map_path, mode='r')
+
+        stilism = dust_map['stilism']
+        cube = stilism['cube_datas']
+        ebv = cube[:]
+
+        steps = cube.attrs['gridstep_values']
+        center = cube.attrs['sun_position'] * steps
+
+        self.x0 = np.arange(ebv.shape[0])*steps[0] - center[0]
+        self.y0 = np.arange(ebv.shape[1])*steps[1] - center[1]
+        self.z0 = np.arange(ebv.shape[2])*steps[2] - center[2]
+
+        if version == '19':
+            # At least for version 2019 of the map
+            self.x0 += 0.5*steps[0]
+            self.y0 += 0.5*steps[1]
+            self.z0 += 0.5*steps[2] 
+
+        self.rgi = interpolate.RegularGridInterpolator((self.x0, self.y0, self.z0), ebv, \
+                                                        bounds_error=False, fill_value=0.)
+        print("Setup complete.")
+
+    def find_max_distance(self, l=0, b=0, dists=np.array([])):
+
+        """Find the maximum distance in a supplied array that is within the
+        bounds of the extinction samples
+
+        Returns the maximum distance, the boolean array indicating which
+        distances are inside the cube, and the xyz coordinates along this
+        sight line from the distances.
+
+        Args:
+            l (float): Galactic latitude of the pointing.
+            b (array): Galactic longitude of the pointing.
+            dists (array): Array of distances
+
+        """
+
+        # Split out from get_ebv_lallement so that we can call this piece
+        # from other routines (e.g. when finding a sensible maximum
+        # distance)
+
+        if np.size(dists) < 1:
+            return 0., np.array([]), np.array([])
+
+        xyz = gal_to_xyz(l, b, dists)
+        bInCube = (np.abs(xyz[0]) < self.x0[-1]) & \
+                (np.abs(xyz[1]) < self.y0[-1]) & \
+                (np.abs(xyz[2]) < self.z0[-1])
+
+        distMax = np.max(dists[bInCube])
+
+        return distMax, bInCube, xyz
+        
+    def get_ebv(self, l,b,dist,dmin=0.5,dstep=5, \
+                        distances=np.array([]), returnAv=False,\
+                        Verbose=False):
+
+        # Now accespts an array of distances. The stepsize is computed
+        # element by element.
+        
+        # d_interp = np.arange(dmin,dist+0.1*dstep,dstep)
+        if np.size(distances) < 1:
+            d_interp = generate_distances(dist, dmin, dstep)
+        else:
+            d_interp = distances
+
+        # generate the distance step allowing for nonunuform bins
+        d_step = d_interp - np.roll(d_interp, 1)
+        d_step[0] = d_step[1] # the zeroth element needs specifying
+            
+        # distance, boolean and xyz refactored to a separate method
+        distMax, bInCube, xyz_interp = self.find_max_distance(l, b, d_interp)
+
+        
+        #xyz_interp = gal_to_xyz(l,b,d_interp)
+        
+        # WIC - update the selection for objects being inside the cube
+        #bInCube = (np.abs(xyz_interp[0])<x0[-1]) & \
+        #          (np.abs(xyz_interp[1])<y0[-1]) & \
+        #          (np.abs(xyz_interp[2])<z0[-1])
+        
+        #distMax = np.max(d_interp[bInCube])
+
+        # The regular grid interpolator returns zero for points outside
+        # the cube, which do not impact the sum along the sight line.
+        ebv_interp = self.rgi(xyz_interp)
+
+        if np.sum(~bInCube) > 0 and Verbose:
+            print("get_ebv_lallement INFO - some distances are beyond the cutoff %.1f pc for this sight line" % (distMax))
+        
+        #if not np.all( (np.abs(xyz_interp[0])<x0[-1]) & (np.abs(xyz_interp[1])<y0[-1]) & (np.abs(xyz_interp[2])<z0[-1])):
+        #    max_dist = z0[-1] / math.sin(math.radians(b))
+
+        #    # try max_dist_x
+        #    max_dist_x = x0[-1] / math.cos(math.radians(b))
+
+        #    max_dist = np.min([max_dist, max_dist_x])
+            
+        #    print("Queried distance of {}pc is out of the map. Maximum distance for this sightline is {:.1f}pc.".format(dist,max_dist))
+        #    return np.nan, d_interp
+        #ebv_interp = rgi(xyz_interp)
+        if returnAv:
+            fact = self.EBV2A[self.version]
+            if Verbose:
+                print("Converting map version {} to Av (fact={})".format(self.version,fact))
+            Av = np.cumsum(ebv_interp*d_step) * fact
+            return Av, d_interp, distMax # it is an E(B-V), must multiply by Rv
+
+        fact = self.A2EBV[self.version]
+        if Verbose:
+            print("Converting map version {} to E(B-V) (fact={})".format(self.version,fact))
+        ebv = np.cumsum(ebv_interp*d_step) * fact
+        return ebv, d_interp, distMax # it is an E(B-V), must multiply by Rv
 
 def gal_to_xyz(l,b,dist):
     l_rad = math.radians(l)
